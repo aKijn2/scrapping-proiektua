6f5e5f10ad311bd789f58c987c9d77570f652b74
aKijn2 <101109227+aKijn2@users.noreply.github.com>
2023-10-17 13:29:11 +0200

    Add files via upload

---
+++ src/test/java/com/si/scrapping/scrapping/ScrappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.si.scrapping.scrapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
