f40ff3477cef74e3f4dc29f4a5c495ccce6b13e3
aKijn2 <mrkinder20041010@gmail.com>
2023-10-20 10:20:52 +0000

    v2

--- src/test/java/com/si/scrapping/scrapping/ScrappingApplicationTests.java
+++ src/test/java/com/si/scrapping/scrapping/ScrappingApplicationTests.java
@@ -1,7 +1,13 @@
 package com.si.scrapping.scrapping;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 @SpringBootTest
 class ScrappingApplicationTests {
@@ -10,4 +16,26 @@ class ScrappingApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void redisConnectiontest(){
+		        JedisPool pool = new JedisPool("172.17.0.3", 6379);
+			
+
+				
+        try (Jedis jedis = pool.getResource()) {
+            // Store & Retrieve a simple string
+            jedis.set("foo", "bar");
+            System.out.println(jedis.get("foo")); // prints bar
+            
+            // Store & Retrieve a HashMap
+            Map<String, String> hash = new HashMap<>();;
+            hash.put("name", "John");
+            hash.put("surname", "Smith");
+            hash.put("company", "Redis");
+            hash.put("age", "29");
+            jedis.hset("user-session:123", hash);
+            System.out.println(jedis.hgetAll("user-session:123"));
+            // Prints: {name=John, surname=Smith, company=Redis, age=29}
+        }
+}
 }
